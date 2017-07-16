e233d903414f1d098b793afeaa5f8ed9e0c2766b
york yao <yaoao12306@outlook.com>
2017-07-16 09:17:21 +0800

    new release

--- dist/config.js
+++ dist/config.js
@@ -1,55 +1,14 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.applications = [
-    {
-        repositoryName: "deploy-robot-demo",
-        hookSecret: "test secret",
-        pullRequest: {
-            getTestUrl(port, pullRequestId) {
-                return `http://106.15.39.164:9000/${pullRequestId}/`;
-            },
-            mergedCommand: "/opt/scripts/pr_merged.sh",
-            openedCommand: "/opt/scripts/pr_opened.sh",
-            closedCommand: "/opt/scripts/pr_closed.sh",
-            updatedCommand: "/opt/scripts/pr_updated.sh",
-        },
-        commentActions: [
-            {
-                // deploy
-                filter: (comment, author) => comment.indexOf("robot") >= 0
-                    && comment.indexOf("deploy") >= 0
-                    && comment.indexOf("please") >= 0
-                    && ["plantain-00"].findIndex(a => a === author) >= 0,
-                command: "/opt/scripts/deploy.sh",
-                gotMessage: "正在部署...",
-                doneMessage: "部署已完成，https://deploy-demo.yorkyao.xyz/",
-            },
-        ],
-    },
-    {
-        repositoryName: "deploy-robot-backend-demo",
-        hookSecret: "test secret",
-        pullRequest: {
-            getTestUrl(port, pullRequestId) {
-                return `http://106.15.39.164:${port}/api/`;
-            },
-            mergedCommand: "/opt/backend_scripts/pr_merged.sh",
-            openedCommand: "/opt/backend_scripts/pr_opened.sh",
-            closedCommand: "/opt/backend_scripts/pr_closed.sh",
-            updatedCommand: "/opt/backend_scripts/pr_updated.sh",
-        },
-        commentActions: [
-            {
-                // deploy
-                filter: (comment, author) => comment.indexOf("robot") >= 0
-                    && comment.indexOf("deploy") >= 0
-                    && comment.indexOf("please") >= 0
-                    && ["plantain-00"].findIndex(a => a === author) >= 0,
-                command: "/opt/backend_scripts/deploy.sh",
-                gotMessage: "正在部署...",
-                doneMessage: "部署已完成，https://deploy-demo.yorkyao.xyz/api/",
-            },
-        ],
-    },
-];
-exports.localeName = "zh-cn";
+const defaultConfig = {
+    applications: [],
+    localeName: "zh-cn",
+};
+try {
+    // tslint:disable-next-line:no-var-requires
+    require("../deploy-robot.config.js")(defaultConfig);
+}
+catch (error) {
+    // tslint:disable-next-line:no-console
+    console.log(error);
+}
+module.exports = defaultConfig;
